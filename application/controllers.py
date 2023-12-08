f42e742fd29c84b1e4aefba7236eb2bc3c72607e
AdapaPrasanthi <prasanthiadapa2003@gmail.com>
2023-12-08 18:46:33 +0530

    changes done

--- application/controllers.py
+++ application/controllers.py
@@ -21,7 +21,30 @@ def user_login():
         if user.role_id != 2:
             flash('You are not an user. Try login correctly.')
             return redirect(url_for('home'))
+        return redirect(url_for('user_home'))
+
+@app.route("/user_home", methods=["GET", "POST"])
+def user_home():
+    if request.method == "GET":
         return render_template('user_home.html')
+    
+
+@app.route("/user_signup", methods=["GET", "POST"])
+def user_signup():
+    if request.method == "GET":
+        return render_template('user_signup.html')
+    
+
+@app.route("/admin_login", methods=["GET", "POST"])
+def admin_login():
+    if request.method == "GET":
+        return render_template('admin_login.html')
+    
+
+@app.route("/creator", methods=["GET", "POST"])
+def creator():
+    if request.method == "Get":
+        return render_template('creator.html')
 
 
 
