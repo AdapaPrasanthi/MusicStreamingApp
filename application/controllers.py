c7cb939ee148b21e3a790f8cbf1e807d28499279
shabarish1403 <shabarish.14b@gmail.com>
2023-12-09 02:36:47 +0530

    changes done

--- application/controllers.py
+++ application/controllers.py
@@ -4,17 +4,13 @@ from flask import current_app as app
 from application.models import *
 from .database import db
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def home():
-    if request.method == 'GET':
-        return render_template("home.html")
-
+    return render_template("home.html")
        
 @app.route("/user_login", methods=["GET", "POST"])
 def user_login():
-    if request.method == 'GET':
-        return render_template("user_login.html")
-    elif request.method == 'POST':
+    if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
         user = User.query.filter_by(email=email, password=password).first()
@@ -25,23 +21,15 @@ def user_login():
             flash('You are not an user. Try login correctly.')
             return redirect(url_for('home'))
         return redirect(url_for('user_home'))
-
-@app.route("/user_home", methods=["GET", "POST"])
-def user_home():
-    if request.method == "GET":
-        return render_template('user_home.html')
-    
+    return render_template("access.html",login=True)
 
 @app.route("/user_signup", methods=["GET", "POST"])
 def user_signup():
-    if request.method == "GET":
-        return render_template('user_signup.html')
-    elif request.method == "POST":
+    if request.method == "POST":
         email = request.form['email']
         name = request.form['name']
         password = request.form['password']
         confirm_password = request.form['confirm_password']
-
         if password != confirm_password:
             flash('Password not matched')
             return redirect(url_for('user_signup'))
@@ -50,13 +38,11 @@ def user_signup():
         db.session.commit()
         flash('Signed up successful')
         return redirect(url_for('user_login'))
-    
+    return render_template('access.html',login=False)
 
 @app.route("/admin_login", methods=["GET", "POST"])
 def admin_login():
-    if request.method == "GET":
-        return render_template('admin_login.html')
-    elif request.method == "POST":
+    if request.method == "POST":
         email = request.form['email']
         password = request.form['password']
         user = User.query.filter_by(email=email, password=password).first()
@@ -67,16 +53,19 @@ def admin_login():
             flash('You are not an admin. Try login correctly.')
             return redirect(url_for('home'))
         return redirect(url_for('admin_home'))
+    return render_template('access.html',admin=True)
     
-@app.route("/admin_home", methods=["GET","POST"])
+@app.route("/user_home", methods=["GET"])
+def user_home():
+    return render_template('user_home.html')
+    
+@app.route("/admin_home", methods=["GET"])
 def admin_home():
-    if request.method == "GET":
-        return render_template('admin_home.html')
+    return render_template('admin_home.html')
 
-@app.route("/creator", methods=["GET", "POST"])
+@app.route("/creator", methods=["GET"])
 def creator():
-    if request.method == "Get":
-        return render_template('creator.html')
+    return render_template('creator_home.html')
 
 
 
