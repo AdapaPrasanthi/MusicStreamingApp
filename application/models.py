570eeaadd292720e98947efa46caf34bbd3cfc67
AdapaPrasanthi <prasanthiadapa2003@gmail.com>
2023-12-08 12:33:44 +0530

    changes done

---
+++ application/models.py
@@ -0,0 +1,43 @@
+from .database import db
+
+import datetime
+
+class Role(db.Model):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    users = db.relationship('User', backref='role', cascade  = 'all,delete-orphan')
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    name = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    active = db.Column(db.Boolean, default=True)
+    role_id = db.Column(db.Integer, db.ForiegnKey('role.id'), nullable=False)
+    albums = db.relationship('Album', backref='user', cascade ='all,delete-orphan')
+
+class Album(db.Model):
+    __tablename__ = 'album'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    genre = db.Column(db.String, nullable=False)
+    artist = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    songs = db.relationship('Song', backref='album', cascade ='all,delete-orphan')
+
+
+class Song(db.Model):
+    __tablename__ = 'song'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    lyrics = db.Column(db.String)
+    duration = db.Column(db.Integer)
+    date_created = db.Column(db.DateTime, default=datetime.datetime.now)
+    album_id = db.Column(db.Integer, db.ForeignKey('album.id', nullable=False))
+    
+
+
+
+    
---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask
+from flask_restful import Resource, Api
+from application.config import LocalDevelopmentConfig
+from application.database import db
+
+app = None
+api = None
+
+def create_app():
+    app = Flask(__name__, template_folder="templates")
+    app.secret_key = 'Secret Key'
+    if os.getenv('ENV', "development") == "production":
+      raise Exception("Currently no production config is setup.")
+    else:
+      print("Staring Local Development")
+      app.config.from_object(LocalDevelopmentConfig)
+    db.init_app(app)
+    app.app_context().push()
+    print("db initialized")
+    db.create_all()
+    api = Api(app)
+    app.app_context().push()
+    print("app created successfully")
+    return app, api
+
+app, api = create_app()
+
+# Import all the controllers so they are loaded
+from application.controllers import *
+
+
+
+if __name__ == '__main__':
+  # Run the Flask app
+  app.run()
